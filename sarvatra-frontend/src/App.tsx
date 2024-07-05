34d04138a1aeed6b852e287ecbd6f67f01da3624
AkshatJha21 <akshatj2134@gmail.com>
2024-07-05 10:14:36 +0530

    router added

--- sarvatra-frontend/src/App.tsx
+++ sarvatra-frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path='/' element={<LandingPage />}/>
+            <Route path='/about' element={<AboutPage />}/>
+            <Route path='/contact' element={<ContactPage />}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
--- sarvatra-frontend/src/components/Navbar.tsx
+++ sarvatra-frontend/src/components/Navbar.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-        Navbar
+      Navbar
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
