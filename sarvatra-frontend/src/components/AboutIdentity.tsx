d16974cb6870aacc864fae5da8f5b53a0b4b23b1
AkshatJha21 <akshatj2134@gmail.com>
2024-07-06 08:45:48 +0530

    finishing touches

--- sarvatra-frontend/src/components/AboutIdentity.tsx
+++ sarvatra-frontend/src/components/AboutIdentity.tsx
@@ -10,7 +10,7 @@ const AboutIdentity = () => {
             alt="identity"
             className="absolute right-0 h-full object-cover z-10"
             initial={{ x: "0%", y: 0, width: "100%" }}
-            whileInView={{ x: "0%", y: "50px", width: "50%", height: "585px"}}
+            whileInView={{ x: "0%", y: "50px", width: "50%", height: "585px", marginRight: "30px"}}
             viewport={{ amount: "all" }}
             transition={{ duration: 0.75 }}
         />
@@ -19,7 +19,7 @@ const AboutIdentity = () => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1}}
             viewport={{ amount: "all" }}
-            transition={{ duration: 1, delay: 1 }}
+            transition={{ duration: 0.75, delay: 1 }}
         >
             <h1 className='text-4xl font-gotu mb-6'>The Identity</h1>
             <p className='w-[90%]'>The symbol for Sarvatva encapsulates the concept of "wholeness," derived from its Sanskrit roots. Nestled within the wave's curves are two dots, symbolizing the dual aspects of a unified whole. One dot represents supreme consciousness, while the other signifies individual consciousness. The harmonious placement of these dots within the wave underscores their interconnectedness, illustrating that true wholeness arises from the balanced interplay between universal and individual consciousness.</p>
