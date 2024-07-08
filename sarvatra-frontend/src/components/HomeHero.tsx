8d6fc8d1c1281f7a5e697fe081196698396c276d
AkshatJha21 <akshatj2134@gmail.com>
2024-07-08 18:44:43 +0530

    hero section of landing page layout done

--- sarvatra-frontend/src/components/HomeHero.tsx
+++ sarvatra-frontend/src/components/HomeHero.tsx
@@ -1,15 +1,40 @@
-import vid from "../assets/land-vid-bg.png"
+
+import { useEffect, useState } from "react";
+import vid from "../assets/land-vid-bg.png";
 
 const HomeHero = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > window.innerHeight * 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
   return (
-    <div className="relative h-[105vh] font-gotu">
+    <div className="relative h-[105vh] font-gotu bg-black">
         <img 
-            className="absolute top-0 left-0 w-full h-full object-cover"
+            className="absolute top-0 left-0 w-full h-full object-cover opacity-90"
             src={vid}
-            alt="bg-video"
         />
+        <div className={`absolute inset-0 flex flex-col items-center justify-center transition-all duration-1000 ${scrolled ? 'bg-black bg-opacity-70' : ''}`}>
+            <h1 className={`text-white text-2xl md:text-4xl transition-all duration-1000 mb-10 ${scrolled ? 'md:translate-y-[-10vh] text-white' : 'mt-72 sm:mt-40 md:mt-48'}`}>
+              तद् एकम्  | That One
+            </h1>
+            <div className={`text-white font-avenir text-sm md:text-xl opacity-0 flex flex-col gap-y-6 md:gap-y-8 text-justify w-[60%] md:w-[50%] transition-all duration-1000 ${scrolled ? 'md:translate-y-[-10vh] opacity-100' : ''}`}>
+                <p>
+                  Going 13.8 Billion years back in time, when all creation originated from the supreme consciousness or “That one”, our collection ponders upon what existed before the universe was born and how all creation came about. Each design is an intention, to manifest this cosmic mystery, into tangibility.
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default HomeHero
+export default HomeHero;
--- sarvatra-frontend/src/pages/ContactPage.tsx
+++ sarvatra-frontend/src/pages/ContactPage.tsx
@@ -1,10 +1,60 @@
 import Navbar from "../components/Navbar"
-
+import banner from "../assets/contactus.png"
 
 const ContactPage = () => {
   return (
     <div>
       <Navbar />
+      <img src={banner} alt="" className="w-full"/>
+      <div className="flex flex-col items-center mt-44">
+        <h1 className="text-4xl font-gotu">Contact Us</h1>
+        <div className="flex my-8 text-xl mx-2 font-avenir font-medium items-center">
+          <label htmlFor="category" className="p-2">I am an: </label>
+          <select className="text-center px-4 py-2 border-t-2 border-b-2 border-black" name="category" id="userCategory">
+            <option value="none">Select option</option>
+            <option value="architect">Architect</option>
+            <option value="interiorDesigner">Interior Designer</option>
+            <option value="others">Others</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-2 my-8 gap-x-24 gap-y-10 w-[90%] mx-[72px]">
+          <input type="text" placeholder="Company / Name *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+          <input type="text" placeholder="Country *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+          <input type="text" placeholder="First Name *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+          <input type="text" placeholder="Last Name *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+          <input type="text" placeholder="Email *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+          <input type="text" placeholder="Phone No. *" className="placeholder:text-[#393939] font-avenir font-light text-xl px-4 py-2 border-b border-[#393939]"/>
+        </div>
+        <div className="my-4 flex flex-col w-[90%] justify-start">
+          <h1 className="font-avenir text-xl py-2">Product of Interest</h1>
+          <div className="flex space-x-10">
+            <div className="flex py-2 font-avenir font-light text-lg">
+              <input type="checkbox" id="product1" value="product1"/>
+              <label htmlFor="product1" className="px-4">Product One</label>
+            </div>
+            <div className="flex py-2 font-avenir font-light text-lg">
+              <input type="checkbox" id="product2" value="product2"/>
+              <label htmlFor="product2" className="px-4">Product Two</label>
+            </div>
+            <div className="flex py-2 font-avenir font-light text-lg">
+              <input type="checkbox" id="product3" value="product3"/>
+              <label htmlFor="product3" className="px-4">Product Three</label>
+            </div>
+            <div className="flex py-2 font-avenir font-light text-lg">
+              <input type="checkbox" id="product4" value="product4" className="text-neutral-800"/>
+              <label htmlFor="product4" className="px-4">Product Four</label>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col my-4 w-[90%] justify-start">
+          <textarea name="message" id="message" placeholder="How can we help? *" className="border font-avenir border-[#393939] p-2 resize-none" rows={8}></textarea>
+          <div className="flex my-2 font-avenir font-light text-lg">
+            <input type="checkbox" id="notifications" value="true"/>
+            <label htmlFor="notification" className="px-4">I would like to recieve the Sarvatva newsletter with updates, inspiration and more</label>
+          </div>
+          <button className="w-[100px] my-2 transition-all rounded-full font-avenir text-center hover:opacity-75 px-4 py-2 bg-black text-white">Submit</button>
+        </div>
+      </div>
     </div>
   )
 }
