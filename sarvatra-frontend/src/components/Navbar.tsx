1ac190c8d5559bfca0c09f806f0a078d3b8cb3fe
AkshatJha21 <akshatj2134@gmail.com>
2024-07-08 10:31:04 +0530

    navbar perfected

--- sarvatra-frontend/src/components/Navbar.tsx
+++ sarvatra-frontend/src/components/Navbar.tsx
@@ -21,17 +21,17 @@ const Navbar = () => {
 
   return (
     // Translucent background like Apple website
-    <div className={`font-gotu fixed flex px-4 py-2 items-center justify-between top-0 left-0 w-full z-20 transition-colors duration-300 ${scrolled ? 'bg-white text-black' : 'bg-transparent text-white'}`}>
-      <img src={scrolled ? darkLogo : logo} alt="sarvatva" width={44}/>
+    <div className={`font-gotu fixed flex px-4 py-2 items-center justify-between top-0 left-0 w-full z-20 transition-colors duration-300 ${scrolled ? 'text-black bg-white bg-opacity-85 backdrop-blur-sm' : 'backdrop-blur-sm text-white'}`}>
+      <img src={scrolled ? darkLogo : logo} alt="sarvatva" width={63}/>
       <div className='flex space-x-2 md:gap-x-4 text-sm md:text-md'>
         <Link to={'/'}>
-          <p className='py-2 md:px-4 hover:bg-black hover:bg-opacity-30 rounded transition-all'>Collection</p>
+          <p className={`py-4 md:px-4 text-[18px] rounded transition-all ${scrolled ? 'hover:bg-black hover:bg-opacity-10' : 'hover:bg-black hover:bg-opacity-30'}`}>Collection</p>
         </Link>
         <Link to={'/about'}>
-          <p className='py-2 md:px-4 hover:bg-black hover:bg-opacity-30 rounded transition-all'>About Us</p>
+          <p className={`py-4 md:px-4 text-[18px] rounded transition-all ${scrolled ? 'hover:bg-black hover:bg-opacity-10' : 'hover:bg-black hover:bg-opacity-30'}`}>About Us</p>
         </Link>
         <Link to={'/contact'}>
-          <p className='py-2 md:px-4 hover:bg-black hover:bg-opacity-30 rounded transition-all'>Contact Us</p>
+          <p className={`py-4 md:px-4 text-[18px] rounded transition-all ${scrolled ? 'hover:bg-black hover:bg-opacity-10' : 'hover:bg-black hover:bg-opacity-30'}`}>Contact Us</p>
         </Link>
       </div>
     </div>
