ae1b040fdf5be0a1d7795e3a0d820c3c68a5a762
AkshatJha21 <akshatj2134@gmail.com>
2024-07-10 12:00:41 +0530

    hero animation bug

--- sarvatra-frontend/src/components/HomeHero.tsx
+++ sarvatra-frontend/src/components/HomeHero.tsx
@@ -1,9 +1,10 @@
-
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import vid from "../assets/landing-vid.mp4";
 
 const HomeHero = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showTranslation, setShowTranslation] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -14,8 +15,15 @@ const HomeHero = () => {
             }
         };
 
+        const timeout = setTimeout(() => {
+          setShowTranslation(true);
+        }, 700); // Adjust delay as needed
+    
         window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+          clearTimeout(timeout);
+        };
     }, []);
   return (
     <div className="relative h-[105vh] font-gotu bg-black">
@@ -27,9 +35,14 @@ const HomeHero = () => {
             loop
         />
         <div className={`absolute inset-0 flex flex-col items-center justify-center transition-all duration-1000 ${scrolled ? 'bg-black bg-opacity-70' : ''}`}>
-            <h1 className={`text-white text-2xl md:text-4xl transition-all duration-1000 mb-10 ${scrolled ? 'md:translate-y-[-10vh] text-white' : 'mt-72 sm:mt-40 md:mt-48'}`}>
-              तद् एकम्  | That One
-            </h1>
+            <div className="flex flex-row">
+              <h1 className={`shlok text-white text-2xl md:text-4xl transition-all duration-1000 mb-10 ${scrolled ? 'md:translate-y-[-10vh] text-white' : 'mt-72 sm:mt-40 md:mt-48'}`}>
+                तद् एकम् &nbsp;
+              </h1>
+              {showTranslation && (
+                <h1 className={`meaning text-white text-2xl md:text-4xl transition-all duration-1000 mb-10 ${scrolled ? 'md:translate-y-[-10vh] text-white' : 'mt-72 sm:mt-40 md:mt-48'}`}>| That One</h1>
+              )}
+            </div>
             <div className={`text-white font-avenir text-sm md:text-xl opacity-0 flex flex-col gap-y-6 md:gap-y-8 text-center w-[60%] md:w-[50%] transition-all duration-1000 ${scrolled ? 'md:translate-y-[-10vh] opacity-100' : ''}`}>
                 <p>
                   Going 13.8 Billion years back in time, when all creation originated from the supreme consciousness or “That one”, our collection ponders upon what existed before the universe was born and how all creation came about. Each design is an intention, to manifest this cosmic mystery, into tangibility.
