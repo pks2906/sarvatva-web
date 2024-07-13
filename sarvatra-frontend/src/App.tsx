25d2665364e9459c9400c4cd2ae6b6bf5c7ee194
AkshatJha21 <akshatj2134@gmail.com>
2024-07-13 10:10:59 +0530

    reset scroll position updated

--- sarvatra-frontend/src/App.tsx
+++ sarvatra-frontend/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <div>
           <Routes>
             <Route path='/' element={<LandingPage />}/>
