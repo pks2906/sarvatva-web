3b35b36c0db0a997b60da7c4309d1dbfeab66346
AkshatJha21 <akshatj2134@gmail.com>
2024-07-14 16:34:30 +0530

    layered card added

---
+++ sarvatra-frontend/src/data.ts
@@ -0,0 +1,51 @@
+import img1 from "./assets/display1.jpg"
+import img2 from "./assets/display2.jpg"
+import img3 from "./assets/display3.jpg"
+import img4 from "./assets/display4.jpg"
+import img5 from "./assets/display5.jpg"
+import img6 from "./assets/display6.jpg"
+import img7 from "./assets/display7.jpg"
+
+export const products = [
+    {
+        title: "CONSIOUSNESS",
+        description: "The Side Table born from a single log of wood, it symbolises the beginning of manifestation from the supreme consciousness.",
+        src: img1,
+        align: "right"
+    },
+    {
+        title: "DESIRE",
+        description: "The Coffee Table like a seed, is a metaphor for desire, which is the first thought, on the journey from consciousness to manifestation.",
+        src: img2,
+        align: "left"
+    },
+    {
+        title: "MANIFESTATION",
+        description: "The Console Table depicts the manifestation of our desire, with an imagery alike the graceful unfolding of the sacred lotus flower.",
+        src: img3,
+        align: "right"
+    },
+    {
+        title: "HURDLE",
+        description: "The Lounge Chair’s back rest, depict the dilemmas we face. Whereas, the seating, is that calm space that helps one take a stepback, and plan the bigger leap.",
+        src: img4,
+        align: "left"
+    },
+    {
+        title: "HARMONY",
+        description: "The Couch’s backrest keeps the two seating spaces intact, depicting the state of balance between colliding thoughts, which makes space for peace.",
+        src: img5,
+        align: "right"
+    },{
+        title: "ILLUSION",
+        description: "The lines of the Console Cabinet through seem wavy but are actually straight, creating an illusion, which is also the reality of creation. It’s all, still a mystery!",
+        src: img6,
+        align: "left"
+    },
+    {
+        title: "SURRENDER",
+        description: "The flowing lines on the Bed’s Backrest with no definite start or end, depict that phase of our life where we finally realise, creation is beyond our control and we let it take its natural flow.",
+        src: img7,
+        align: "right"
+    }
+]
--- sarvatra-frontend/src/pages/LandingPage.tsx
+++ sarvatra-frontend/src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import vid from "../assets/landing-vid-ezgif.com-reverse-video.mp4"
 import HeroParallax from "../components/HeroParallax";
 import ProductCard from "../components/ProductCard";
+import { products } from '../data.ts'
 
 const heroSubhead = "Going 13.8 Billion years back in time, when all creation originated from the supreme consciousness or “That one”, our collection ponders upon what existed before the universe was born and how all creation came about. Each design is an intention, to manifest this cosmic mystery, into tangibility."
 
@@ -12,7 +13,11 @@ const LandingPage = () => {
     <div>
       <Navbar />
       <HeroParallax vidUrl={vid} heading="तद् एकम् | That One" subheading={heroSubhead}/>
-      <ProductCard />
+      <div>
+        {products.map((product, index) => {
+          return <ProductCard key={index} {...product}/>
+        })}
+      </div>
       <AboutTeam />
       <Footer />
     </div>
