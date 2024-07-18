43c299bdaf98cf9e1f3199da9d1bf29c7829c197
AkshatJha21 <akshatj2134@gmail.com>
2024-07-18 16:06:52 +0530

    about us left/right changes

--- sarvatra-frontend/src/components/AboutLeft.tsx
+++ sarvatra-frontend/src/components/AboutLeft.tsx
@@ -3,16 +3,18 @@ interface AboutLeftProps {
     title: string;
     para1: string;
     para2: string;
+    para3?: string;
 }
 
-const AboutLeft = ({ imgSrc ,title, para1, para2 }: AboutLeftProps) => {
+const AboutLeft = ({ imgSrc ,title, para1, para2, para3 }: AboutLeftProps) => {
   return (
     <div className="h-screen">
         <div className="flex flex-col-reverse md:flex-row items-center md:ml-[72px] md:gap-x-6 text-[#131313]">
-            <div className='flex flex-col justify-center w-[90%] md:w-[50%] mx-auto'>
+            <div className='flex flex-col justify-center w-[90%] md:w-[50%] md:px-14 mx-auto'>
                 <h1 className='font-staatliches text-4xl md:text-6xl tracking-[0.125em] mt-[47px] mb-4'>{title}</h1>
                 <p className='font-avenir font-light text-[16px] md:text-[20px] leading-[1.5rem] md:leading-9 my-4 md:mt-[28px] md:w-[75%]'>{para1}</p>
                 <p className='font-avenir font-light text-[16px] md:text-[20px] leading-[1.5rem] md:leading-9 my-4 md:mt-[28px] md:w-[75%] '>{para2}</p>
+                <p className='font-avenir font-light text-[16px] md:text-[20px] leading-[1.5rem] md:leading-9 my-4 md:mt-[28px] md:w-[75%] '>{para3}</p>
             </div>
             <div 
                 className='h-[40vh] w-[100vw] md:h-[100vh] md:w-[60%]'
--- sarvatra-frontend/src/pages/AboutPage.tsx
+++ sarvatra-frontend/src/pages/AboutPage.tsx
@@ -9,23 +9,25 @@ import AboutHero from "../components/AboutHero"
 import AboutLeft from "../components/AboutLeft"
 import AboutRight from '../components/AboutRight'
 
-const para1 = "Wholeness is not a treasure to be sought in the external world but a natural instinct of our inner world. It is a craft waiting to be nurtured and revealed in the flow of everyday living."
-const para2 = "Wholeness is a journey of inner rediscovery. Let Sarvatva guide you on this transformative journey, one exquisite creation at a time."
-const para3 = "The Vedas are ancient sacred texts of India, considered to be the oldest surviving spiritual texts in the world. It is believed that the knowledge of the Vedas was revealed to sages through deep meditation."
-const para4 = "The Vedas explore metaphysical concepts such as the nature of reality, existence, and ultimate truth. By reconnecting with this ancient wisdom, we can navigate the complexities of life with simplicity in the 21st Century."
-const para5 = "With a legacy spanning 400 years, India's wood carving craft is a living testament to our rich cultural heritage. Sarvatva is rejuvenating this craft through a harmonious fusion of new-age design and age-old craftsmanship."
-const para6 = "Each piece serves as a bridge where the ancient meets the future. Through this reimagining, Sarvatva invites you to experience the profoundness of India's artisanal legacy that enriches contemporary spaces."
-const para7 = "The Indian Handicraft Industry is the bread winner for 7 million artisans across the geography of India. We are currently witnessing the last generation of artisans, as the new generation is opting out of it for better economic opportunities. "
-const para8 = "As changemakers, we foresee a huge economic growth that lies in the fusion of craft, design & technology. It might be our final call to change this narrative and keep the legacy alive for & through the generations to come. "
+const para1 = "We believe that wholeness is not found in the external universe but is a natural instinct of inner universe."
+const para2 = "Wholeness is a craft waiting to be nurtured and revealed in the flow of everyday living."
+const para21 = 'Let Sarvatva guide you on this journey of inner rediscovery, one creation at a time.'
+const para3 = "The Vedas are considered to be the oldest surviving spiritual texts on Earth, compiled almost 3000 years ago."
+const para4 = "The Vedas illuminate metaphysical concepts such as the nature of reality, existence, and ultimate truth."
+const para41 = "By reconnecting with this ancient wisdom, we can navigate the complexities of the 21st Century with simplicity."
+const para5 = "We are reimagining a 400 year old wood carving craftsmanship through its fusion with new-age design serving as a bridge between the ancient and the future."
+const para6 = "Sarvatva invites you to experience the profoundness of India's artisanal legacy that enriches contemporary spaces."
+const para7 = "The Indian Handicraft Industry is home to 7 million artisans. We are currently witnessing the last generation of artisans, as the new generation is opting out of it for better economic opportunities."
+const para8 = "Sarvatva foresees an opportunity that lies in the fusion of craft, design & technology. It is an intention to change this narrative and keep the legacy alive for & through the generations to come. "
 
 const AboutPage = () => {
   return (
     <div>
         <Navbar />
         <AboutHero />
-        <AboutLeft imgSrc={img1} title="REDISCOVER" para1={para1} para2={para2}/>
+        <AboutLeft imgSrc={img1} title="REDISCOVER" para1={para1} para2={para2} para3={para21}/>
         <AboutRight imgSrc={img2} title='REIMAGINE' para1={para5} para2={para6}/>
-        <AboutLeft imgSrc={img4} title="RECONNECT" para1={para3} para2={para4}/>
+        <AboutLeft imgSrc={img4} title="RECONNECT" para1={para3} para2={para4} para3={para41}/>
         <AboutRight imgSrc={img3} title='HIGHER PURPOSE' para1={para7} para2={para8}/>
         <AboutTeam />
         <Footer />
