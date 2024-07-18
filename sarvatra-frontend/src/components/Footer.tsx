016603870bf9cbcd79d65d9e17c6e23e32f8f82b
AkshatJha21 <akshatj2134@gmail.com>
2024-07-18 12:45:30 +0530

    footer consistent

--- sarvatra-frontend/src/components/Footer.tsx
+++ sarvatra-frontend/src/components/Footer.tsx
@@ -7,43 +7,44 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className="relative bottom-0 w-full h-auto pt-28 bg-[#131313]">
-        <div className="flex flex-col md:flex-row justify-between mx-[72px] px-10">
+        <div className="flex flex-col md:flex-row justify-between md:mx-[72px] px-10">
             <div className="flex flex-col w-9/10 md:w-1/2">
-                <h1 className="font-staatliches tracking-[0.125em] text-[#ECE6D7] text-xl md:text-2xl">Join our Community</h1>
-                <p className="font-avenir text-[#ECE6D7] mt-10">Subscribe to our mailing list</p>
-                <input type="text" placeholder="Email address" className="bg-transparent placeholder:text-[#ECE6D7] text-[#ECE6D7] font-avenir placeholder:font-light border-b border-[#ECE6D7] py-2 mt-10 w-full md:w-[75%]"/>
-                <button className="flex w-[37.5%] px-4 py-3 bg-[#ECE6D7]/90 justify-center rounded-md font-avenir mt-6 hover:opacity-80 transition tracking-widest ease-in-out">SIGN UP</button>
+                <h1 className="font-staatliches tracking-[0.125em] opacity-80 text-[#EDE6D6] text-xl md:text-2xl">Join our Community</h1>
+                <p className="font-avenir text-[#EDE6D6] opacity-80 mt-10">Subscribe to our mailing list</p>
+                <input type="text" placeholder="Email address" className="bg-transparent opacity-80 placeholder:text-[#EDE6D6] text-[#EDE6D6] font-avenir placeholder:font-light border-b border-[#EDE6D6] py-2 mt-10 w-full md:w-[75%]"/>
+                <button className="flex w-1/2 md:w-[37.5%] px-4 py-2 text-[12px] opacity-80 bg-white/10 border-[0.5px] border-[#EDE6D6] text-[#EDE6D6] justify-center rounded-md font-medium font-avenir mt-6 hover:bg-[#EDE6D6] hover:text-[#131313] transition tracking-widest ease-in-out">SIGN UP</button>
             </div>
             <div className="flex flex-col w-9/10 md:w-1/3 mt-12 sm:mt-0">
-                <h1 className="font-staatliches tracking-[0.125em] text-[#ECE6D7] text-xl md:text-2xl">Quick Links</h1>
+                <h1 className="font-staatliches tracking-[0.125em] opacity-80 text-[#EDE6D6] text-xl md:text-2xl">Quick Links</h1>
                 <div className="mt-10">
                     <Link to={'/about'}>
-                        <p className="font-avenir text-[#ECE6D7] mb-6 hover:underline">Crafting Wholeness</p>
+                        <p className="font-avenir text-[#EDE6D6] opacity-80 mb-6 hover:underline">Crafting Wholeness</p>
                     </Link>
                     <Link to={'/'}>
-                        <p className="font-avenir text-[#ECE6D7] mb-6 hover:underline">Vedic Furniture</p>
+                        <p className="font-avenir text-[#EDE6D6] opacity-80 mb-6 hover:underline">Vedic Furniture</p>
                     </Link>
                     <Link to={'/collection'}>
-                        <p className="font-avenir mb-6 text-[#ECE6D7] hover:underline cursor-pointer">Lookbook</p>
+                        <p className="font-avenir mb-6 text-[#EDE6D6] opacity-80 hover:underline cursor-pointer">Lookbook</p>
                     </Link>
                     <a href="https://medium.com/@iteesha18" target='_blank'>
-                        <p className="font-avenir mb-6 text-[#ECE6D7] hover:underline">Blog</p>
+                        <p className="font-avenir mb-6 text-[#EDE6D6] opacity-80 hover:underline">Blog</p>
                     </a>
                 </div>
             </div>
             <div className="flex flex-col w-9/10 md:w-1/3 mt-12 sm:mt-0">
-                <h1 className="font-staatliches tracking-[0.125em] text-[#ECE6D7] text-xl md:text-2xl">Let's Connect</h1>
-                <p className="mt-10 text-[#ECE6D7] font-avenir">connect@sarvatva.co.in</p>
-                <h1 className="font-staatliches tracking-[0.125em] text-[#ECE6D7] text-xl md:text-2xl mt-10">Follow Us</h1>
+                <h1 className="font-staatliches tracking-[0.125em] opacity-80 text-[#EDE6D6] text-xl md:text-2xl">Let's Connect</h1>
+                <p className="mt-10 text-[#ECE6D7] opacity-80 font-avenir">connect@sarvatva.co.in</p>
+                <p className="mt-6 text-[#ECE6D7] opacity-80 font-avenir tracking-wider">+91 - 8106865897</p>
+                <h1 className="font-staatliches tracking-[0.125em] opacity-80 text-[#EDE6D6] text-xl md:text-2xl mt-10">Follow Us</h1>
                 <div className="grid grid-cols-3 mt-10">
                     <a href="https://pin.it/4FwD7UyAQ" className='cursor-auto'>
-                        <img src={pi} alt="Pinterest" className='h-[42px] hover:opacity-75 transition-opacity cursor-pointer'/>
+                        <img src={pi} alt="Pinterest" className='h-[42px] opacity-80 hover:opacity-60 transition-opacity cursor-pointer'/>
                     </a>
                     <a href="https://www.linkedin.com/company/sarvatva-craftingwholeness/?viewAsMember=true" className='cursor-auto'>
-                        <img src={li} alt="Linkedin" className='h-[42px] hover:opacity-75 transition-opacity cursor-pointer'/>
+                        <img src={li} alt="Linkedin" className='h-[42px] opacity-80 hover:opacity-60 transition-opacity cursor-pointer'/>
                     </a>
                     <a href="https://www.instagram.com/_sarvatva_?igsh=czE2b29nNnE4YnFm&utm_source=qr" className='cursor-auto'>
-                        <img src={ig} alt="Instagram" className='h-[42px] hover:opacity-75 transition-opacity cursor-pointer'/>
+                        <img src={ig} alt="Instagram" className='h-[42px] opacity-80 hover:opacity-60 transition-opacity cursor-pointer'/>
                     </a>
                 </div>
             </div>
--- sarvatra-frontend/src/components/LandingAbout.tsx
+++ sarvatra-frontend/src/components/LandingAbout.tsx
@@ -16,7 +16,7 @@ const LandingAbout = () => {
                 <h1 className='font-staatliches text-4xl md:text-6xl tracking-[0.125em] mb-4 md:mb-[8px]'>THE CRAFT OF WHOLENESS</h1>
                 <p className='font-avenir font-light md:text-[20px] mt-8 md:my-10 md:w-[75%] leading-9'>Our creations are more than furniture; they are meticulously crafted invitations to experience an intrinsic sense of wholeness that resides in our inner world.</p>
                 <Link to={'/about'}>
-                    <button className='font-avenir border-[0.5px] text-[12px] md:text-sm py-2 px-4 w-1/2 md:w-[200px] tracking-widest opacity-80 bg-white/10 text-[#EDE6D6] font-medium rounded-md border-[#EDE6D6] shadow-lg backdrop-blur-sm hover:text-[#131313] hover:bg-[#EDE6D6] transition-all md:mt-[28px] mt-8' >EXPLORE MORE</button>
+                    <button className='font-avenir border-[0.5px] text-[10px] md:text-sm py-2 px-4 w-1/2 md:w-[200px] tracking-widest opacity-80 bg-white/10 text-[#EDE6D6] font-medium rounded-md border-[#EDE6D6] shadow-lg backdrop-blur-sm hover:text-[#131313] hover:bg-[#EDE6D6] transition-all md:mt-[28px] mt-4' >EXPLORE MORE</button>
                 </Link>
             </div>
         </div>
