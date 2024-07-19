ef17c5d69d2e759e908160cf69fb05b9f67d20e4
AkshatJha21 <akshatj2134@gmail.com>
2024-07-19 20:33:26 +0530

    LandingAbout spacing update

--- sarvatra-frontend/src/components/AboutCollection.tsx
+++ sarvatra-frontend/src/components/AboutCollection.tsx
@@ -37,7 +37,7 @@ const AboutCollection = () => {
                         </p>
                     </div>
                 </div>
-                <div onClick={scrollToNext} className='absolute bottom-3 flex flex-col left-[45%] md:left-[50%] mx-auto animate-bounce -space-y-6 opacity-80 cursor-pointer'>
+                <div onClick={scrollToNext} className='absolute bottom-0 flex flex-col left-[45%] md:left-[50%] mx-auto animate-bounce -space-y-6 opacity-80 cursor-pointer'>
                     <svg className="w-10 h-10 text-[#EDE6D6]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 15 7-7 7 7"/>
                     </svg>
