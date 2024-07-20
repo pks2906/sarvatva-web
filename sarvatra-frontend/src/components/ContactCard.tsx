2b0d9e37b7fd5af69a1962babfa82c0aa77c3e9a
AkshatJha21 <akshatj2134@gmail.com>
2024-07-20 20:17:37 +0530

    contact us opacity updated

--- sarvatra-frontend/src/components/ContactCard.tsx
+++ sarvatra-frontend/src/components/ContactCard.tsx
@@ -10,15 +10,15 @@ const ContactCard = () => {
                 <p className="font-avenir font-light text-lg md:text-xl text-[#131313]/50">Over a cup of Tea...</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 my-2 md:my-8 gap-x-4 md:gap-x-24 gap-y-2 md:gap-y-10 w-[90%]">
-                <input type="text" placeholder="Company / Name *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
-                <input type="text" placeholder="Country *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
-                <input type="text" placeholder="First Name *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
-                <input type="text" placeholder="Last Name *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
-                <input type="text" placeholder="Email *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
-                <input type="text" placeholder="Phone No. *" className="placeholder:text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="Company / Name *" className=" text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="Country *" className="text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="First Name *" className="text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="Last Name *" className="text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="Email *" className="text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
+                <input type="text" placeholder="Phone No. *" className="text-[#131313] font-avenir font-light md:text-xl px-4 py-2 border-b border-[#131313]" />
               </div>
               <div className="flex flex-col my-4 w-[90%]">
-                <textarea name="message" id="message" placeholder="Tell us more about your vision? We would love to collaborate with you *" className="border rounded-md text-sm md:text-lg font-avenir placeholder:italic border-[#393939] p-2 resize-none" rows={8}></textarea>
+                <textarea name="message" id="message" placeholder="Tell us more about your vision? We would love to collaborate with you *" className="border rounded-md text-sm md:text-lg font-avenir placeholder:italic border-[#131313] p-2 resize-none" rows={8}></textarea>
                 <div className="flex my-2 font-avenir font-light text-sm md:text-lg">
                   <input type="checkbox" id="notifications" value="true" />
                   <label htmlFor="notifications" className="px-4">I would like to receive the Sarvatva newsletter with updates, inspiration and more</label>
