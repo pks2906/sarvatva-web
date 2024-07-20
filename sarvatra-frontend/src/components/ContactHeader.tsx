900b114ea4b80b9ddc2907127ab7ac9d1e4e628d
AkshatJha21 <akshatj2134@gmail.com>
2024-07-20 17:32:56 +0530

    navbar update

--- sarvatra-frontend/src/components/ContactHeader.tsx
+++ sarvatra-frontend/src/components/ContactHeader.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
-import logo from '../assets/navLight.png';
 import darkLogo from '../assets/logoDark.png';
 
-const Navbar = () => {
+const ContactHeader = () => {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -35,29 +34,29 @@ const Navbar = () => {
     <div className={`font-cormorant fixed flex px-4 items-center justify-between top-0 left-0 w-full z-20 transition-colors duration-300 ${scrolled ? 'bg-white/[12.5%] backdrop-blur-sm shadow-lg' : ''}`}>
       <Link to={'/'}>
       <div className='flex items-center space-x-4 font-avenir font-light text-lg'>
-        <img src={scrolled ? darkLogo : logo} alt="sarvatva" width={240}/>
+        <img src={darkLogo} alt="sarvatva" width={240}/>
       </div>
       </Link>
-      <div className={`md:hidden flex items-center ${scrolled ? '' : 'text-[#EDE6D6]/80'}`}>
+      <div className={`md:hidden flex items-center text-[#131313]`}>
         <button onClick={toggleMenu} className="flex items-center">
           {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
       </div>
       <div className={`hidden md:flex space-x-2 md:gap-x-4 text-sm md:text-md`}>
         <Link to={'/collection'}>
-          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#EDE6D6]/80 hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>Vedic Furniture</p>
+          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#131313] hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>Vedic Furniture</p>
         </Link>
         <Link to={'/about'}>
-          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#EDE6D6]/80 hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>The Craft of Wholeness</p>
+          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#131313] hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>The Craft of Wholeness</p>
         </Link>
         <Link to={'/contact'}>
-          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#EDE6D6]/80 hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>Let's Connect</p>
+          <p className={`py-4 md:px-4 text-[18px] font-semibold rounded transition-all ${scrolled ? 'hover:text-[#EDE6D6]/80 text-[#131313]' : 'text-[#131313] hover:bg-black hover:bg-opacity-30 hover:text-[#EDE6D6]'}`}>Let's Connect</p>
         </Link>
       </div>
       <AnimatePresence>
         {menuOpen && (
           <motion.div
-            className={`md:hidden absolute top-[110%] right-2 transition ${scrolled ? 'bg-white' : 'bg-white/80 backdrop-blur-sm'} shadow-lg border-[0.5px] rounded-md flex flex-col items-start px-6`}
+            className={`md:hidden absolute top-[110%] right-2 transition bg-white/90 backdrop-blur shadow-lg border-[0.5px] rounded-md flex flex-col items-start px-6`}
             initial="hidden"
             animate="visible"
             exit="hidden"
@@ -80,4 +79,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default ContactHeader;
--- sarvatra-frontend/src/pages/ContactPage.tsx
+++ sarvatra-frontend/src/pages/ContactPage.tsx
@@ -1,75 +1,13 @@
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ContactCard from "../components/ContactCard";
+import ContactHeader from "../components/ContactHeader";
 
 const ContactPage = () => {
 
   return (
     <div className="overflow-x-hidden">
-      <Navbar />
+      <ContactHeader />
       <ContactCard />
-      {/* <div className="h-screen">
-        <img src={banner} alt="" className="w-full h-[30vh] md:h-auto object-cover" />
-        <div className="flex flex-col items-center mt-20 px-4 md:px-0">
-          <h1 className="text-3xl md:text-4xl font-staatliches">Let's Connect</h1>
-          <div className="flex flex-col md:flex-row my-8 text-lg md:text-xl font-avenir font-medium items-center">
-            <label htmlFor="category" className="p-2">I am an: </label>
-            <select 
-              className="text-center px-4 py-2 border-t-2 border-b-2 border-black" 
-              name="category" 
-              id="userCategory" 
-              value={selectedOption} 
-              onChange={handleOptionChange}
-            >
-              <option value="none">Select option</option>
-              <option value="architect">Architect</option>
-              <option value="interiorDesigner">Interior Designer</option>
-              <option value="others">Others</option>
-            </select>
-          </div>
-          {selectedOption !== "none" && (
-            <>
-              <div ref={formRef} className="grid grid-cols-1 md:grid-cols-2 my-8 gap-x-4 md:gap-x-24 gap-y-4 md:gap-y-10 w-full md:w-[90%]">
-                <input type="text" placeholder="Company / Name *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-                <input type="text" placeholder="Country *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-                <input type="text" placeholder="First Name *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-                <input type="text" placeholder="Last Name *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-                <input type="text" placeholder="Email *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-                <input type="text" placeholder="Phone No. *" className="placeholder:text-[#393939] font-avenir font-light text-lg md:text-xl px-4 py-2 border-b border-[#393939]" />
-              </div>
-              <div className="my-4 flex flex-col w-full md:w-[90%]">
-                <h1 className="font-avenir text-lg md:text-xl py-2">Product of Interest</h1>
-                <div className="grid grid-cols-2 md:flex md:space-x-10">
-                  <div className="flex py-2 font-avenir font-light text-md md:text-lg">
-                    <input type="checkbox" id="product1" value="product1" />
-                    <label htmlFor="product1" className="px-4">Product One</label>
-                  </div>
-                  <div className="flex py-2 font-avenir font-light text-md md:text-lg">
-                    <input type="checkbox" id="product2" value="product2" />
-                    <label htmlFor="product2" className="px-4">Product Two</label>
-                  </div>
-                  <div className="flex py-2 font-avenir font-light text-md md:text-lg">
-                    <input type="checkbox" id="product3" value="product3" />
-                    <label htmlFor="product3" className="px-4">Product Three</label>
-                  </div>
-                  <div className="flex py-2 font-avenir font-light text-md md:text-lg">
-                    <input type="checkbox" id="product4" value="product4" className="text-neutral-800" />
-                    <label htmlFor="product4" className="px-4">Product Four</label>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col my-4 w-full md:w-[90%]">
-                <textarea name="message" id="message" placeholder="How can we help? *" className="border font-avenir border-[#393939] p-2 resize-none" rows={8}></textarea>
-                <div className="flex my-2 font-avenir font-light text-md md:text-lg">
-                  <input type="checkbox" id="notifications" value="true" />
-                  <label htmlFor="notifications" className="px-4">I would like to receive the Sarvatva newsletter with updates, inspiration and more</label>
-                </div>
-                <button className="w-full md:w-[100px] my-2 transition-all rounded-full font-avenir text-center hover:opacity-75 px-4 py-2 bg-black text-white">Submit</button>
-              </div>
-            </>
-          )}
-        </div>
-      </div> */}
       <Footer />
     </div>
   );
