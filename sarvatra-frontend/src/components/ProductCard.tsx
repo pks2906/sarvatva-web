fee7012234aefe3199387607288adaf08eff6842
AkshatJha21 <akshatj2134@gmail.com>
2024-07-21 12:48:56 +0530

    contact us working

--- sarvatra-frontend/src/components/ProductCard.tsx
+++ sarvatra-frontend/src/components/ProductCard.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import emailjs from '@emailjs/browser';
 
 interface ProductProps {
   title: string;
@@ -9,9 +10,13 @@ interface ProductProps {
   src: string;
   align: string;
   mobile: string;
+  productName: string;
 }
 
-const PhoneInputField = () => {
+const PhoneInputField = ({ setPhoneNumber }: { setPhoneNumber: (phone: string) => void }) => {
+  const handlePhoneChange = (value: string) => {
+    setPhoneNumber(value);
+  };
   return (
     <div className="flex flex-col my-4">
       <PhoneInput
@@ -42,13 +47,79 @@ const PhoneInputField = () => {
         containerStyle={{
           width: '100%',
         }}
+        onChange={handlePhoneChange}
       />
     </div>
   );
 };
 
-const PopupForm = ({ onClose }: { onClose: () => void }) => {
+const PopupForm = ({ onClose, productName }: { onClose: () => void, productName: string }) => {
+  const form = useRef<HTMLFormElement>(null);
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm('service_0ezieoa', 'template_trvb30j', form.current, { publicKey: 'XcW2jNaUhvrtFXOHS' })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+            alert('Email sent successfully');
+            onClose();
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+            alert('Failed to send email');
+          }
+        );
+    }
+  };
+
   return (
+    // <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10">
+    //   <motion.div 
+    //     className="bg-white bg-opacity-10 flex flex-col p-4 md:p-8 rounded-md shadow-lg w-[90%] md:w-[60%] backdrop-blur-md"
+    //     initial={{ opacity: 0, scale: 0.8 }}
+    //     animate={{ opacity: 1, scale: 1 }}
+    //     exit={{ opacity: 0, scale: 0.8 }}
+    //   >
+    //     <div className="flex justify-between items-center mb-0 md:mb-8">
+    //       <h1 className="font-cormorant text-[#EDE6D6]/80 font-bold text-lg md:text-xl">Request More Information</h1>
+    //       <button 
+    //         className="text-[#EDE6D6]/80 font-medium text-lg md:text-xl"
+    //         onClick={onClose}  
+    //       >
+    //         &times;
+    //       </button>
+    //     </div>
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8 my-4">
+    //       <input type="text" placeholder="First Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80"/>
+    //       <input type="text" placeholder="Last Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80"/>
+    //     </div>
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+    //       <select name="communication" id="mode" className="bg-transparent font-avenir font-bold text-start py-2 border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80">
+    //         <option value="none">Mode of Communication</option>
+    //         <option value="email">Email</option>
+    //         <option value="sms">WhatsApp</option>
+    //       </select>
+    //     </div>
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+    //       <input type="email" placeholder="Email" className="font-avenir bg-transparent py-2 font-bold border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80 placeholder:text-[#EDE6D6]/80"/>
+    //     </div>
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-1 mb-4">
+    //       {/* <input type="tel" placeholder="Phone No." className="font-avenir bg-transparent py-2 font-bold border-b border-white text-white placeholder:text-white"/> */}
+    //       <PhoneInputField />
+    //     </div>
+    //     <div className="my-4">
+    //       <textarea placeholder="Your Message" className="placeholder:text-[#EDE6D6]/80 font-bold border rounded-md text-[#EDE6D6]/80 font-avenir bg-transparent w-full border-[#EDE6D6]/80 p-2 resize-none" rows={4}></textarea>
+    //     </div>
+    //     <div className="md:mt-4">
+    //       <button onClick={onClose} className="w-full md:w-[100px] transition-all rounded-md font-avenir font-medium text-center hover:opacity-75 px-4 py-2 bg-[#EDE6D6]/80 border-[0.5px] text-[#131313]/80 tracking-wider text-sm">SUBMIT</button>
+    //     </div>
+    //   </motion.div>
+    // </div>
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10">
       <motion.div 
         className="bg-white bg-opacity-10 flex flex-col p-4 md:p-8 rounded-md shadow-lg w-[90%] md:w-[60%] backdrop-blur-md"
@@ -65,36 +136,39 @@ const PopupForm = ({ onClose }: { onClose: () => void }) => {
             &times;
           </button>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8 my-4">
-          <input type="text" placeholder="First Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80"/>
-          <input type="text" placeholder="Last Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80"/>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
-          <select name="communication" id="mode" className="bg-transparent font-avenir font-bold text-start py-2 border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80">
-            <option value="none">Mode of Communication</option>
-            <option value="email">Email</option>
-            <option value="sms">WhatsApp</option>
-          </select>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
-          <input type="email" placeholder="Email" className="font-avenir bg-transparent py-2 font-bold border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80 placeholder:text-[#EDE6D6]/80"/>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-1 mb-4">
-          {/* <input type="tel" placeholder="Phone No." className="font-avenir bg-transparent py-2 font-bold border-b border-white text-white placeholder:text-white"/> */}
-          <PhoneInputField />
-        </div>
-        <div className="my-4">
-          <textarea placeholder="Your Message" className="placeholder:text-[#EDE6D6]/80 font-bold border rounded-md text-[#EDE6D6]/80 font-avenir bg-transparent w-full border-[#EDE6D6]/80 p-2 resize-none" rows={4}></textarea>
-        </div>
-        <div className="md:mt-4">
-          <button onClick={onClose} className="w-full md:w-[100px] transition-all rounded-md font-avenir font-medium text-center hover:opacity-75 px-4 py-2 bg-[#EDE6D6]/80 border-[0.5px] text-[#131313]/80 tracking-wider text-sm">SUBMIT</button>
-        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="hidden" name="product_name" value={productName} />
+          <input type="hidden" name="phone_number" value={phoneNumber} />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8 my-4">
+            <input type="text" name="first_name" placeholder="First Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80" required />
+            <input type="text" name="last_name" placeholder="Last Name" className="bg-transparent placeholder:text-[#EDE6D6]/80 text-[#EDE6D6]/80 font-avenir font-bold py-2 border-b border-[#EDE6D6]/80" required />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+            <select name="communication" id="mode" className="bg-transparent font-avenir font-bold text-start py-2 border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80" required>
+              <option value="">Mode of Communication</option>
+              <option value="email">Email</option>
+              <option value="sms">WhatsApp</option>
+            </select>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+            <input type="email" name="user_email" placeholder="Email" className="font-avenir bg-transparent py-2 font-bold border-b border-[#EDE6D6]/80 text-[#EDE6D6]/80 placeholder:text-[#EDE6D6]/80" required />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-1 mb-4">
+            <PhoneInputField setPhoneNumber={setPhoneNumber}/>
+          </div>
+          <div className="my-4">
+            <textarea name="message" placeholder="Your Message" className="placeholder:text-[#EDE6D6]/80 font-bold border rounded-md text-[#EDE6D6]/80 font-avenir bg-transparent w-full border-[#EDE6D6]/80 p-2 resize-none" rows={4}></textarea>
+          </div>
+          <div className="md:mt-4">
+            <button type="submit" className="w-full md:w-[100px] transition-all rounded-md font-avenir font-medium text-center hover:opacity-75 px-4 py-2 bg-[#EDE6D6]/80 border-[0.5px] text-[#131313]/80 tracking-wider text-sm">SUBMIT</button>
+          </div>
+        </form>
       </motion.div>
     </div>
   )
 }
 
-const ProductCard = ({ title, description, src, align, mobile }: ProductProps) => {
+const ProductCard = ({ title, description, src, align, mobile, productName }: ProductProps) => {
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
@@ -151,7 +225,7 @@ const ProductCard = ({ title, description, src, align, mobile }: ProductProps) =
           </div>
         </div>
         <AnimatePresence>
-          {isFormOpen && <PopupForm onClose={handleCloseForm} />}
+          {isFormOpen && <PopupForm onClose={handleCloseForm} productName={productName} />}
         </AnimatePresence>
       </div>
     );
@@ -182,7 +256,7 @@ const ProductCard = ({ title, description, src, align, mobile }: ProductProps) =
         </div>
       </div>
       <AnimatePresence>
-        {isFormOpen && <PopupForm onClose={handleCloseForm} />}
+        {isFormOpen && <PopupForm onClose={handleCloseForm} productName={productName}/>}
       </AnimatePresence>
     </div>
   )
--- sarvatra-frontend/src/data.ts
+++ sarvatra-frontend/src/data.ts
@@ -15,6 +15,8 @@ import mob7 from './assets/mobile6.jpg'
 
 export const products = [
     {
+        id: 1,
+        productName: 'Side Table',
         title: "Consiousness",
         description: `The <strong>Side Table</strong> born from a single log of wood, it symbolises the beginning of manifestation from the supreme consciousness.`,
         src: img1,
@@ -22,6 +24,8 @@ export const products = [
         mobile: mob1
     },
     {
+        id: 2,
+        productName: 'Coffee Table',
         title: "Desire",
         description: "The <strong>Coffee Table</strong> like a seed, is a metaphor for desire, which is the first thought, on the journey from consciousness to manifestation.",
         src: img2,
@@ -29,6 +33,8 @@ export const products = [
         mobile: mob2
     },
     {
+        id: 3,
+        productName: 'Console Table',
         title: "Manifestation",
         description: "The <strong>Console Table</strong> depicts the manifestation of our desire, with an imagery alike the graceful unfolding of the sacred lotus flower.",
         src: img3,
@@ -36,6 +42,8 @@ export const products = [
         mobile: mob3
     },
     {
+        id: 4,
+        productName: 'Lounge Chair',
         title: "Hurdle",
         description: "The <strong>Lounge Chair’s</strong> back rest, depict the dilemmas we face. Whereas, the seating, is that calm space that helps one take a stepback, and plan the bigger leap.",
         src: img4,
@@ -43,12 +51,17 @@ export const products = [
         mobile: mob4
     },
     {
+        id: 5,
+        productName: 'Couch',
         title: "Harmony",
         description: "The <strong>Couch’s</strong> backrest keeps the two seating spaces intact, depicting the state of balance between colliding thoughts, which makes space for peace.",
         src: img5,
         align: "right-[5%]",
         mobile: mob5
-    },{
+    },
+    {
+        id: 6,
+        productName: 'Console Cabinet',
         title: "Illusion",
         description: "The lines of the <strong>Console Cabinet</strong> through seem wavy but are actually straight, creating an illusion, which is also the reality of creation. It’s all, still a mystery!",
         src: img6,
@@ -56,6 +69,8 @@ export const products = [
         mobile: mob6
     },
     {
+        id: 7,
+        productName: 'Bed',
         title: "Surrender",
         description: "The flowing lines on the <strong>Bed’s Backrest</strong> with no definite start or end, depict that phase of our life where we finally realise, creation is beyond our control and we let it take its natural flow.",
         src: img7,
