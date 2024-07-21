4a40f72b6d8712de8b23854ce01d5dcc6dcacdd7
AkshatJha21 <akshatj2134@gmail.com>
2024-07-21 20:48:00 +0530

    hero section updated

--- sarvatra-frontend/src/components/LandingHero.tsx
+++ sarvatra-frontend/src/components/LandingHero.tsx
@@ -1,7 +1,20 @@
-import vid from "../assets/tad-ekam.mp4"
-import { motion } from "framer-motion"
+import img from '../assets/hero-banner.jpg'
+import mob from '../assets/hero-mobile.jpg'
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 const LandingHero = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
   const scrollToNext = () => {
     window.scrollBy({
       top: window.innerHeight,
@@ -9,21 +22,40 @@ const LandingHero = () => {
     });
   };
   return (
-    <div className="h-screen relative">
-        <video 
-            src={vid} 
-            autoPlay 
-            loop 
-            muted
-            playsInline
-            className='absolute inset-0 bg-black scale-125 md:scale-100 md:object-cover w-full h-full'
-        />
+    <div 
+      className="h-screen relative"
+      style={{
+        background: `url(${isMobile ? mob : img})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover'
+      }}
+    >
+        <div className='flex h-full items-center justify-center text-[#EDE6D6]/80 bg-[#131313]/30'>
+          <div className='flex flex-col text-center gap-y-4 mt-4 md:mt-8'> 
+            <motion.h1 
+              className='font-cormorant text-4xl md:text-7xl'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.1 }}
+            >
+              Tad Ekam
+            </motion.h1>
+            <motion.p 
+              className='font-avenir text-balance md:text-[20px] tracking-wider font-light'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.1, delay: 1 }}
+            >
+              Inspired by the Rig Veda
+            </motion.p>
+          </div>
+        </div>
         <motion.div 
           onClick={scrollToNext} 
           className='absolute bottom-3 flex flex-col items-center w-full mx-auto animate-bounce -space-y-6 opacity-80 cursor-pointer'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 2, delay: 5 }}
+          transition={{ duration: 1, delay: 2 }}
         >
             <svg className="w-8 h-8 md:w-10 md:h-10 text-[#EDE6D6]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 15 7-7 7 7"/>
